e6b7d4be10831789ba24788fa627e51746cdc699
wenkey-everest <muni.v.g@everest.engineering>
2023-04-06 21:32:51 +0530

    Remove repeated error cases

--- internal/core/services/service.go
+++ internal/core/services/service.go
@@ -16,42 +16,21 @@ func NewSareeService(repository ports.SareeRepository) *SareeService {
 }
 
 func (s *SareeService) FindAll() ([]domain.Saree, error) {
-
-	sarees, err := s.repository.FindAll()
-	if err != nil {
-		return nil, err
-	}
-	return sarees, nil
+	return s.repository.FindAll()
 }
 
 func (s *SareeService) Find(id string) (domain.Saree, error) {
-	saree, err := s.repository.Find(id)
-	if err != nil {
-		return domain.Saree{}, err
-	}
-	return saree, nil
+	return s.repository.Find(id)
 }
 
 func (s *SareeService) Save(saree domain.Saree) (domain.Saree, error) {
-	saree, err := s.repository.Save(saree)
-	if err != nil {
-		return domain.Saree{}, err
-	}
-	return saree, nil
+	return s.repository.Save(saree)
 }
 
 func (s *SareeService) Update(id string, saree domain.Saree) (domain.Saree, error) {
-	saree, err := s.repository.Update(id, saree)
-	if err != nil {
-		return domain.Saree{}, err
-	}
-	return saree, nil
+	return s.repository.Update(id, saree)
 }
 
 func (s *SareeService) Delete(id string) error {
-	err := s.repository.Delete(id)
-	if err != nil {
-		return err
-	}
-	return nil
+	return s.repository.Delete(id)
 }

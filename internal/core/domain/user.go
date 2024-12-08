3bc4ecea847102575e7ffcc481a58ee3dd4331ff
wenkey <g.m.venkateshmuni@gmail.com>
2024-12-08 23:34:43 +0100

    feat: Add User Model

---
+++ internal/core/domain/user.go
@@ -0,0 +1,12 @@
+package domain
+
+type User struct {
+	ID        string `json:"id" bson:"_id"`
+	Username  string `json:"username" bson:"username"`
+	Password  string `json:"password" bson:"password"`
+	Email     string `json:"email" bson:"email"`
+	Phone     string `json:"phone" bson:"phone"`
+	Role      string `json:"role" bson:"role"`
+	FirstName string `json:"firstName" bson:"firstName"`
+	LastName  string `json:"lastName" bson:"lastName"`
+}
